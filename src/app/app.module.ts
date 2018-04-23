de8c42990ce22b60a9252f0dc9f727399c797c7e
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2018-04-23 13:17:33 +0200

    Fixed a bug and cleaned up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import {AngularFireModule} from 'angularfire2';
-import {environment} from '../environments/environment';
-import { MapsComponent } from './maps/maps.component';
-import { AlbumsComponent } from './albums/albums.component';
-import { UserComponent } from './user/user.component';
-import { SharedComponent } from './shared/shared.component';
-import { AuthComponent } from './auth/auth.component';
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MapsComponent,
-    AlbumsComponent,
-    UserComponent,
-    SharedComponent,
-    AuthComponent,
-    LandingComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
