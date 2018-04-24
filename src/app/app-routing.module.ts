da54c2dc8cb0e3caaaf05e06701ac30a525cdc5e
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2018-04-24 10:38:06 +0200

    Setting up the structure of the app
    Added routes, set up stuff and is now pretty ready

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {AlbumsListComponent} from './albums/albums-list/albums-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+
+const routes: Routes = [
+  { path: 'albums', component: AlbumsListComponent },
+  { path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatSidenavModule} from '@angular/material';
+import { AppRoutingModule } from './/app-routing.module';
+import {AlbumsModule} from './albums/albums.module';
+import {AuthModule} from './auth/auth.module';
 
 
 @NgModule({
@@ -8,7 +13,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule,
+    AlbumsModule,
+    AuthModule,
+    MatSidenavModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
