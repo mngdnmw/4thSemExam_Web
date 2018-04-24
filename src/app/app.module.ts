0535ede9d3c92d4084dd09c3b0fec5e8f0a8f369
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2018-04-24 14:51:43 +0200

    Added icon module and more layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatSidenavModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
 import { AppRoutingModule } from './/app-routing.module';
 import {AlbumsModule} from './albums/albums.module';
 import {AuthModule} from './auth/auth.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
@@ -21,8 +22,10 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     AlbumsModule,
     AuthModule,
     MatSidenavModule,
-    FlexLayoutModule
-
+    FlexLayoutModule,
+    MatIconModule,
+    MatListModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SignupComponent } from './signup/signup.component';
+import { MarginIconComponent } from './margin-icon/margin-icon.component';
+import {MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
-  declarations: [ToolbarComponent, SignupComponent]
+  declarations: [ToolbarComponent, SignupComponent, MarginIconComponent],
+  exports: [MarginIconComponent]
 })
 export class SharedModule { }
