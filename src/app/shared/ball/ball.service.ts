33e73919a16a93aff6555cdc9f66561eb0186946
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2018-05-09 10:49:15 +0200

    Updated so we are ready for Angular's timestamps in snapshots

--- src/app/shared/ball/ball.service.ts
+++ src/app/shared/ball/ball.service.ts
@@ -6,7 +6,9 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class BallService {
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore) {
+    afs.firestore.settings({ timestampsInSnapshots: true });
+  }
 
   public getAllBalls(): Observable<Ball[]> {
   return this.afs.collection<Ball>('balls').valueChanges();
