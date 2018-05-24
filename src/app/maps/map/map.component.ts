2eb38d32f63e0b903eed903be2b4eb4dbc24e994
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2018-05-24 17:01:24 +0200

    Fixed error

--- src/app/maps/map/map.component.ts
+++ src/app/maps/map/map.component.ts
@@ -48,9 +48,22 @@ export class MapComponent implements OnInit {
 
   // Downloads image(s) from Firebase Storage
   getImages() {
-    this.bs.getImageFromFirebase(this.path, this.imgUrl);
+    this.bs.getImageFromFirebase(this.path).then(
+      url => this.imgUrl = url).catch(
+      function(error) {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/object_not_found':
+            // File doesn't exist
+            console.log('File doesn\'t exist');
+            break;
+          case 'storage/unknown':
+            // Unknown error occurred, inspect the server response
+            console.log('Unknown error');
+            break;
+        }
+      });
   }
 
 }
-
-
