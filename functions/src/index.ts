72a884aad89f90296541cda154282205006b216e
mngdnmw <mtdunmow@gmail.com>
2018-05-28 06:18:54 +0200

    Removed API key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,35 @@
-import * as functions from "firebase-functions";
+'use strict';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-exports.uploadedFileToStorage = functions.storage.object().onFinalize(event => {
-  console.log('something uploaded ',  event);
-})
+const functions = require('firebase-functions');
+const mkdirp = require('mkdirp-promise');
+// Include a Service Account Key to use a Signed URL
+const gcs = require('@google-cloud/storage')({keyFilename: 'service-account-credentials.json'});
+const admin = require('firebase-admin');
+admin.initializeApp();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+
+exports.generateImageDocument = functions.storage.object().onFinalize((object) => {
+  // File and directory paths.
+  const filePath = object.name;
+  const fileName = path.basename(filePath);
+
+  //const fileString = JSON.stringify(fileName);
+  // // Exit if this is triggered on a file that is not an image.
+  //const s = fileString.substring(0, fileString.indexOf('.'));
+  console.log(fileName);
+  //console.log("trim "+fileName.trimRight());
+
+  return;
+  // if (!object.contentType.startsWith('image/')) {
+  //   console.log('This is not an image.');
+  //   return null;
+  // }
+  //
+  // return admin.database().ref('images/${fileName}').update({uid: filePath});
+
+});
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,4 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDzl_c8NLnGcVzn6yWXy-KTYp-ChbtKhy4',
-    authDomain: 'sosomafioso-5c8ef.firebaseapp.com',
-    databaseURL: 'https://sosomafioso-5c8ef.firebaseio.com',
-    projectId: 'sosomafioso-5c8ef',
-    storageBucket: 'sosomafioso-5c8ef.appspot.com',
-    messagingSenderId: '311607148277'
-  }
+
 };
