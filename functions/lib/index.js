92d80d52461aba3f5a9e6ed1e93591cfa1a91892
mngdnmw <mtdunmow@gmail.com>
2018-05-29 00:39:48 +0200

    addUidToImageDocument and deleteImageStored functions works

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -9,21 +9,48 @@ const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-exports.generateImageDocument = functions.storage.object().onFinalize((object) => {
-    // File and directory paths.
+/*
+** Updates the document in firestore with the unique id assigned to the document and paired file in storage.
+ */
+exports.addUidToImageDocument = functions.storage.object().onFinalize((object) => {
     const filePath = object.name;
-    const fileName = path.basename(filePath);
-    //const fileString = JSON.stringify(fileName);
-    // // Exit if this is triggered on a file that is not an image.
-    //const s = fileString.substring(0, fileString.indexOf('.'));
-    console.log(fileName);
-    //console.log("trim "+fileName.trimRight());
-    return;
-    // if (!object.contentType.startsWith('image/')) {
-    //   console.log('This is not an image.');
-    //   return null;
-    // }
-    //
-    // return admin.database().ref('images/${fileName}').update({uid: filePath});
+    let fileName = JSON.stringify(path.basename(filePath));
+    //Checks to see if there is a dot in the filename
+    if (fileName.indexOf('.')) {
+        fileName = fileName.substring(1, fileName.indexOf('.'));
+    }
+    if (!object.contentType.startsWith('image/')) {
+        console.log('This is not an image.');
+        return null;
+    }
+    return admin.firestore().collection(`pictures`).doc(fileName).update({ uid: fileName }).then(function () {
+        console.log('UID in document successfully updated!');
+        return;
+    }).catch(function (err) {
+        console.log('Unable to update UID in document.', err);
+        return null;
+    });
+});
+/*
+** Deletes the image document when the corresponding image file has been deleted from storage.
+ */
+exports.deleteImageStored = functions.storage.object().onDelete((object) => {
+    const filePath = object.name;
+    let fileName = JSON.stringify(path.basename(filePath));
+    //Checks to see if there is a dot in the filename
+    if (fileName.indexOf('.')) {
+        fileName = fileName.substring(1, fileName.indexOf('.'));
+    }
+    if (!object.contentType.startsWith('image/')) {
+        console.log('This is not an image.');
+        return null;
+    }
+    return admin.firestore().collection(`pictures`).doc(fileName).delete().then(function () {
+        console.log('Document successfully deleted!');
+        return;
+    }).catch(function (err) {
+        console.log('Unable to delete document.', err);
+        return null;
+    });
 });
 //# sourceMappingURL=index.js.map
